0ed675cc3338dcb9430b4779771dd09b595767e8
Meniny <meniny@qq.com>
2017-08-21 18:54:31 +0800

    1.0.0

---
+++ Sample/Pods/Target Support Files/ObjectAssociation/ObjectAssociation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ObjectAssociation : NSObject
+@end
+@implementation PodsDummy_ObjectAssociation
+@end
---
+++ Sample/Sample/Sample.swift
@@ -0,0 +1,26 @@
+//
+//  Sample.swift
+//  Sample
+//
+//  Created by Meniny on 2017-08-21.
+//  Copyright © 2017年 Meniny. All rights reserved.
+//
+
+import Foundation
+import ObjectAssociation
+
+extension NSObject {
+  
+  private static let association = ObjectAssociation<String>()
+  // Same as:
+//  private static let association = ObjectAssociation<String>(policy: .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+  
+  public var uniqueIdentifier: String? {
+    get {
+      return NSObject.association[self]
+    }
+    set {
+      NSObject.association[self] = newValue
+    }
+  }
+}
